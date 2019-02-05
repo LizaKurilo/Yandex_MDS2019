8801d335f0d7f5f6ea9c21a164fe07ec564b1590
Lizaveta Kurilo <lizakurilo@gmail.com>
2019-02-05 19:52:46 +0300

    master: add all

---
+++ app/src/main/java/com/yandex/lizakurilo/lizakuriloapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yandex.lizakurilo.lizakuriloapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+    }
+}
